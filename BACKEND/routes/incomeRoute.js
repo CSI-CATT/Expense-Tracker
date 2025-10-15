0abed32d2cd3ff0107ebc8256d72bd6520620036
AasthaOswal <aasthaoswal29@gmail.com>
2025-10-15 09:06:51 +0530

    Added controllers and routes for delete income

--- BACKEND/routes/incomeRoute.js
+++ BACKEND/routes/incomeRoute.js
@@ -4,7 +4,8 @@ const {
     addIncome,
     getAllIncome,
     deleteIncome,
-    downloadIncomeExcel
+    downloadIncomeExcel,
+    deleteAllIncome
 } = require('../controllers/incomeController')
 
 const {isLoggedIn} = require('../middlewares/isLoggedIn')
@@ -13,6 +14,7 @@ const router = express.Router()
 router.post('/addincome',isLoggedIn,addIncome)
 router.get('/getallincome',isLoggedIn,getAllIncome)
 router.get('/downloadincomeexcel',isLoggedIn,downloadIncomeExcel)
+router.delete('/deleteAllIncome',isLoggedIn, deleteAllIncome)
 router.delete('/:id',isLoggedIn,deleteIncome)
 
 module.exports = router;
